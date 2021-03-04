00c15cb6f38b8cbdb294ebc50c4fe4ae4489ea50
dangqiangongchengqianming <dangqiangongcheng@123.com>
2021-03-04 16:15:10 +0800

    说明

--- src/main/java/com/atguigu/git/HelloWorld.java
+++ src/main/java/com/atguigu/git/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("hello world!");
 		System.out.println("test remote pull in eclipse...");
-			
+		String s="可能是eclipse的版本较低，pull和push不上，用命令行的方式也行";	
 	}
 
 }
