33939130205f5a483bb23f95fae67df25c2bf267
suke2021 <79627131+suke2021@users.noreply.github.com>
2021-03-04 15:56:31 +0800

    Update HelloWorld.java

--- src/main/java/com/atguigu/git/HelloWorld.java
+++ src/main/java/com/atguigu/git/HelloWorld.java
@@ -3,6 +3,8 @@ package com.atguigu.git;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("hello world!");
+		System.out.println("test remote pull in eclipse...");
+			
 	}
 
 }
