790c532887250584cae1db8687aa78f69b3a0be7
Administrator <Administrator@KOTIN>
2021-03-05 19:31:24 +0800

    commit

--- src/main/java/com/atguigu/git/HelloWorld.java
+++ src/main/java/com/atguigu/git/HelloWorld.java
@@ -6,6 +6,7 @@ public class HelloWorld {
 		System.out.println("test remote pull in eclipse...");
 		String s="可能是eclipse的版本较低，pull和push不上，用命令行的方式也行";
 		String ss ="20210305";
+		String dd="就是版本的问题。。";
 	}
 
 }
